16a55f176156e28ed7f6e89c17963f9f0056bcbb
misha-solntcev <misha.solntcev@gmail.com>
2022-12-02 12:59:17 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  13. Найти натуральное число из интервала 
+    от a до b с максимальной суммой делителей. */
+
+namespace Lab_3_02_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 100;
+
+            int max = 0;
+            for (int i = a; i <= b; i++)
+            {
+                int sum = 0;                
+                for (int j = 1; j <= b; j++)
+                {
+                    if  (i % j == 0)                    
+                        sum += j;                                        
+                }
+                if (sum > max)
+                {
+                    max = sum;
+                    Console.WriteLine($"i={i}, sum={sum}");
+                }                    
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
